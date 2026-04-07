719a9cf88c94f78ec86af427d2ea1d8c64dee0db
Katarzyna Kowalska <k4s14.kk@gmail.com>
2026-04-07 19:24:53 +0200

    adding mythic point handler functionality

--- scripts/mythicPointHandler.ts
+++ scripts/mythicPointHandler.ts
@@ -3,68 +3,74 @@ import { moduleId } from "./constants.ts";
 
 var isMythicHandlerDialogOpen = false
 
-/**
- * Retrieves the list of party members that are characters
- *
- * @return {Array<Actor>} The list of hero actors.
-*/
-function mythicHeroes(): ActorPF2e[] {
-    return (
-        game.actors?.party?.members
-            .filter((actor) => actor?.isOfType("character"))
-            .filter((actor) => !actor?.system.traits?.value.toString().includes("minion"))
-            .filter((actor) => !actor?.system.traits?.value.toString().includes("eidolon"))
-            .filter((actor) => actor?.system.resources.mythicPoints?.max ? (actor?.system.resources.mythicPoints?.max > 0) : false) || []
-    );
-}
-
-type HeroPointHandlerTemplateActor = {
+// Types
+type MythicHandlerTemplateActor = {
     id: string;
     name: string;
-    radioId: string;
+    checkboxId: string;
     checked: boolean;
 };
 
-type HeroPointHandlerTemplateData = {
+type MythicHandlerTemplateData = {
     instructions: string;
     sessionStart: {
         instructions: string;
         resetTo: string;
-        ignore: string;
+        
     }
 
-    actors: HeroPointHandlerTemplateActor[];
+    killMythicEnemy: {
+        instructions: string;
+        hint: string;
+    }
+
+    followTheCalling: {
+        instructions: string;
+        hint: string;
+    } 
+    bonusPoint: string;
+    ignore: string;
+
+    actors: MythicHandlerTemplateActor[];
     submitShortLabel: string;
     stopShortLabel: string;
 };
 
-function getHeroPointHandlerTemplatePath(): string {
-    return `modules/${moduleId}/templates/mythic-handler.hbs`;
-}
+type MythicPointRecipient = "ALL" | "NONE" | string;
 
+// Main functions
 async function buildHtml(): Promise<string> {
     const actors = mythicHeroes();
     const checkedSet = new Set<number>();
 
 
-    const templateData: HeroPointHandlerTemplateData = {
+    const templateData: MythicHandlerTemplateData = {
         instructions: "Give players mythic points according to their deeds",
         sessionStart: {
-            instructions: "Section 1: Session Start/Mythic deed",
+            instructions: "Session Start/Mythic deed",
             resetTo: "Add 3",
-            ignore: "Do nothing"
         },
+        killMythicEnemy: {
+            instructions: "Killing the mythic enemy",
+            hint: "Selected person, usually the enemy's killer or MVP, will get 2 mythic points, rest will get 1. You MUST select at least 1 person."
+        },
+        followTheCalling: {
+            instructions: "Follow the calling",
+            hint: "Selected person will get 1 mythic point, rewarded once per day for crit success on skill described in character's calling."
+        },
+        bonusPoint: "Give bonus mythic point",
+        ignore: "Do nothing",
         actors: actors.map((actor, index) => {
-            const currentHeroPoints = actor?.isOfType("character") ? actor.system.resources.heroPoints.value : 0;
-            const maxHeroPoints = actor?.isOfType("character") ? actor.system.resources.heroPoints.max : 3;
+            const currentMythicPoints = actor?.isOfType("character") ? actor.system.resources.mythicPoints.value : 0;
+            const maxMythicPoints = actor?.isOfType("character") ? actor.system.resources.mythicPoints.max : 3;
             const ownerName =
                 game.users?.find((u) => !u.isGM && actor?.ownership[u.id] === CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER)
                     ?.name ?? "";
             const ownerSuffix = ownerName ? ` (${ownerName})` : "";
             return {
                 id: actor.id,
-                name: `${actor.name}${ownerSuffix} [${currentHeroPoints}/${maxHeroPoints}]`,
-                radioId: `characters-${index}`,
+                name: `${actor.name}${ownerSuffix} [${currentMythicPoints}/${maxMythicPoints}]`,
+                checkboxId: `characters-${index}`,
                 checked: checkedSet.has(index),
             };
         }),
@@ -73,7 +79,7 @@ async function buildHtml(): Promise<string> {
     };
 
     // @ts-expect-error TODO Fix typing
-    return renderTemplate(getHeroPointHandlerTemplatePath(), templateData);
+    return renderTemplate(getMythicHandlerTemplatePath(), templateData);
 }
 
 export async function mythicPointHandler() {
@@ -81,42 +87,20 @@ export async function mythicPointHandler() {
         return;
     }
     isMythicHandlerDialogOpen = true;
+    const actors = mythicHeroes();
+    var dialogAdjustment = (actors.length - 3)
+    if (dialogAdjustment < 0) dialogAdjustment = 0
     try {
-        const dialogWidth = 375;
-        const dialogHeight = 660;
+        const dialogWidth = 850;
+        const dialogHeight = 630 + dialogAdjustment * 85;
         const title = "Mythic Point Handler";
         const content = await buildHtml();
 
-        const renderHookId = Hooks.on("renderDialogV2", (_app: foundry.applications.api.DialogV2) => {
-            /*if (!app.options.window.contentClasses?.includes("hero-point-handler-window")) {
+        const renderHookId = Hooks.on("renderDialogV2", (app: foundry.applications.api.DialogV2) => {
+            if (!app.options.window.contentClasses?.includes("mythic-point-handler-window")) {
                 return;
             }
             Hooks.off("renderDialogV2", renderHookId);
-
-            const el: HTMLElement = app.element;
-            el.style.top = `${Math.round((window.innerHeight - dialogHeight) / 2)}px`;
-            el.style.left = `${Math.round((window.innerWidth - dialogWidth) / 2)}px`;
-            el.style.width = `${dialogWidth}px`;
-            el.style.height = `${dialogHeight}px`;
-
-            const titleEl = el.querySelector<HTMLElement>(".window-title");
-            if (titleEl) {
-                titleEl.innerHTML = title;
-            }
-
-            const footerButtons = el.querySelectorAll<HTMLButtonElement>("footer button[data-action]");
-            for (const footerButton of footerButtons) {
-                switch (footerButton.dataset.action) {
-                    case "timer":
-                        footerButton.title = submitTooltip;
-                        footerButton.setAttribute("aria-label", submitTooltip);
-                        break;
-                    case "noTimer":
-                        footerButton.title = stopTooltip;
-                        footerButton.setAttribute("aria-label", stopTooltip);
-                        break;
-                }
-            }*/
         });
 
         const result = <boolean | null>await foundry.applications.api.DialogV2.wait({
@@ -130,8 +114,8 @@ export async function mythicPointHandler() {
                 title,
                 resizable: true,
                 positioned: true,
-                contentClasses: ["hero-point-handler-window"],
-                icon: "fa-solid fa-hourglass",
+                contentClasses: ["mythic-point-handler-window"],
+                icon: "fa-solid fa-crown",
             },
             content,
             buttons: [
@@ -140,7 +124,8 @@ export async function mythicPointHandler() {
                     // todo: localization
                     label: "Add Mythic Points",
                     default: true,
-                    callback: async (_event, _button, _dialog) => {
+                    callback: async (_event, button, _dialog) => {
+                        handleDialogResponse(button.form ?? button.closest("form"));
                         return true;
                     },
                 },
@@ -160,3 +145,96 @@ export async function mythicPointHandler() {
     }
 }
 
+// Helper functions
+function getMythicHandlerTemplatePath(): string {
+    return `modules/${moduleId}/templates/mythic-handler.hbs`;
+}
+/**
+ * Retrieves the list of party members that are characters
+ *
+ * @return {Array<Actor>} The list of hero actors.
+*/
+function mythicHeroes(): ActorPF2e[] {
+    return (
+        game.actors?.party?.members
+            .filter((actor) => actor?.isOfType("character"))
+            .filter((actor) => !actor?.system.traits?.value.toString().includes("minion"))
+            .filter((actor) => !actor?.system.traits?.value.toString().includes("eidolon"))
+            .filter((actor) => actor?.system.resources.mythicPoints?.max ? (actor?.system.resources.mythicPoints?.max > 0) : false) || []
+    );
+}
+
+/**
+ * Adds mythic points to the specified actor or all actors.
+ *
+ * @param {number} mythicpoints - The number of mythic points to add.
+ * @param {any} [actorId="ALL"] - The ID of the actor to add mythic points to. If "ALL" is specified, mythic points will be added to all actors.
+ * @return {Promise<void>} - A promise that resolves when the mythic points have been added.
+ */
+async function addMythicPoints(mythicpoints: number, actorId: MythicPointRecipient = "ALL"): Promise<void> {
+    let actors: ActorPF2e[];
+    switch (actorId) {
+        case "ALL":
+            actors = mythicHeroes();
+            break;
+        case "NONE":
+            actors = [];
+            break;
+        default:
+            actors = game.actors?.has(actorId) ? [game.actors.get(actorId, { strict: true })] : [];
+            break;
+    }
+
+    for (const actor of actors) {
+        if (!actor.isOfType("character")) {
+            continue;
+        }
+
+        const system = actor.system;
+        const value = Math.min(system.resources.mythicPoints.value + mythicpoints, actor.system.resources.mythicPoints.max);
+        await actor.update({
+            "system.resources.mythicPoints.value": value,
+        });
+    }
+}
+
+// TODO: Dodac send message
+async function handleDialogResponse(element: ParentNode | null): Promise<void> {
+    if (!element) {
+        return;
+    }
+
+    const sessionStartEl = element.querySelector<HTMLInputElement>("input[name=\"sessionStart\"]:checked");
+    const sessionStart = sessionStartEl ? sessionStartEl.value : "ignore";
+
+    const killMythicEnemyEl = element.querySelectorAll<HTMLInputElement>("input[name=\"killMythicEnemy\"]:checked");
+    const killMythicEnemyActors: string[] = Array.from(killMythicEnemyEl).map((el) => el.value);
+
+    const followTheCallingEl = element.querySelector<HTMLInputElement>("input[name=\"followTheCalling\"]:checked");
+    const followTheCalling = followTheCallingEl ? followTheCallingEl.value : "ignore";
+
+    const bonusPointEls = element.querySelectorAll<HTMLInputElement>("input[name=\"bonusPoint\"]:checked");
+    const bonusPointActors: string[] = Array.from(bonusPointEls).map((el) => el.value);
+
+    if (sessionStart != "ignore") {
+        await addMythicPoints(3);
+        //const message = "Tu bedzie wiadomosc ze start sesji i wgl";
+        //sendMessage(message);
+    } 
+    if (killMythicEnemyActors.length > 0) {
+        await addMythicPoints(1);
+        for (const actor of killMythicEnemyActors) {
+                await addMythicPoints(1,actor);
+        }
+    }
+    if (followTheCalling != "ignore") {
+        await addMythicPoints(1, followTheCalling);
+        // todo: dodac cooldown
+    }
+    if (bonusPointActors.length > 0) {
+        for (const actor of bonusPointActors) {
+                await addMythicPoints(1,actor);
+        }
+    }
+
+}
